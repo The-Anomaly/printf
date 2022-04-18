8d0d85efa0ff4e65c3211a65dd439e00c8642929
The-Anomaly <favouradekogbe@gmail.com>
2022-04-18 22:36:16 +0000

    0-printf

---
+++ printf.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+* _printf - produces output according to a format.
+* @format: print format (a character string)
+* Return: number of characters printed
+*/
+int _printf(const char *format, ...)
+{
+	int i = 0, j = 0;
+	char *str;
+	va_list list;
+
+	va_start(list, format);
+
+	for (; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			switch (format[i + 1])
+			{
+				case 'c':
+					_putchar(va_arg(list, int));
+					break;
+				case 's':
+					j = 0;
+					str = va_arg(list, char *);
+					for (; str[j] != '\0'; j++)
+					{
+						_putchar(str[j]);
+					}
+					break;
+				case '%':
+					_putchar(va_arg(list, int));
+					break;
+			}
+			i++;
+		}
+		else
+		{
+			_putchar(format[i]);
+		}
+	}
+	return (0);
+}
